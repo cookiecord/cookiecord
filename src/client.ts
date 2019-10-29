99f56808eae92efe3fee59dd1af4f4b941f8c50f
RONTheCookie <me@ronthecookie.me>
2019-10-29 13:41:05 +0200

    Add custom arg types

--- src/client.ts
+++ src/client.ts
@@ -1,19 +1,20 @@
 import { Client, ClientOptions } from "discord.js";
 import CommandManager from "./command/commandManager";
 import ListenerManager from "./listener/listenerManager";
-import CommandParserModule from "./command/commandParser";
+import CommandParserModule, { ArgTypes } from "./command/commandParser";
 interface CookiecordOptions {
 	botAdmins?: string[];
+	commandArgumentTypes: ArgTypes
 }
 export default class CookiecordClient extends Client {
 	public commandManager: CommandManager;
 	public listenerManager: ListenerManager;
 	public botAdmins: string[];
-	constructor(opts: CookiecordOptions = {}, discordOpts: ClientOptions = {}) { // look at the example lol wait!!! 
+	constructor(opts: CookiecordOptions = {commandArgumentTypes: {}}, discordOpts: ClientOptions = {}) { // look at the example lol wait!!! 
 		super(discordOpts);
 		this.botAdmins = opts.botAdmins || []; 
 		this.commandManager = new CommandManager();
 		this.listenerManager = new ListenerManager(this);
-		new CommandParserModule(this);
+		new CommandParserModule(this, opts.commandArgumentTypes);
 	}
 }
--- src/command/commandParser.ts
+++ src/command/commandParser.ts
@@ -2,31 +2,36 @@ import Module from "../module";
 import CookiecordClient from "../client";
 import { listener } from "../listener";
 import { Message, MessageMentions } from "discord.js";
-import Permission, { getUserPermission, getMemberPermission } from "./permissions";
-type ArgType = {
+import Permission, {
+    getUserPermission,
+    getMemberPermission
+} from "./permissions";
+export type ArgTypes = {
     [key: string]: (s: string, msg: Message) => unknown;
 };
 const USER_PATTERN = /<@!?(\d+)>/;
-const types: ArgType = {
-    Number: (s) => (isNaN(parseFloat(s)) ? null : parseFloat(s)),
-    String: (s) => s,
-    User: (s, msg) => {
-        const res = USER_PATTERN.exec(s);
-        if (!res) return;
-        return msg.client.users.get(res[1]);
-    },
-    GuildMember: (s, msg) => {
-        const res = USER_PATTERN.exec(s);
-        if (!res || !msg.guild) return;
-        return msg.guild.members.get(res[1]);
-    }
-};
 
 export default class CommandParserModule extends Module {
     public client: CookiecordClient;
-    constructor(client: CookiecordClient) {
+    private types: ArgTypes;
+    constructor(client: CookiecordClient, types: ArgTypes) {
         super(client);
         // console.log(client);
+        this.types = {
+            Number: (s) => (isNaN(parseFloat(s)) ? null : parseFloat(s)),
+            String: (s) => s,
+            User: (s, msg) => {
+                const res = USER_PATTERN.exec(s);
+                if (!res) return;
+                return msg.client.users.get(res[1]);
+            },
+            GuildMember: (s, msg) => {
+                const res = USER_PATTERN.exec(s);
+                if (!res || !msg.guild) return;
+                return msg.guild.members.get(res[1]);
+            }
+        };
+        this.types = Object.assign(this.types, types);
         this.client = client;
     }
     @listener({ event: "message" })
@@ -40,20 +45,26 @@ export default class CommandParserModule extends Module {
         const cmd = this.client.commandManager.cmds.find((c) =>
             c.triggers.includes(cmdTrigger)
         );
-		if (!cmd || !msg.author) return;
-		let perm: Permission;
-		if (msg.member) {
-			perm = getMemberPermission(this.client, msg.member);
-		} else if (msg.author) {
-			perm = getUserPermission(this.client, msg.author);
-		} else {
-			console.error(`message ${msg.id} trigging error in channel ${msg.channel.id}: permission check: both member and author values are falsey`);
-			return msg.reply(":warning: unexpected condition occured: both member and author values are falsey");
-		}
-		if (cmd.permission > perm) {
-			return msg.reply(":warning: unauthorized: elevated permission required.");
-		}
-		
+        if (!cmd || !msg.author) return;
+        let perm: Permission;
+        if (msg.member) {
+            perm = getMemberPermission(this.client, msg.member);
+        } else if (msg.author) {
+            perm = getUserPermission(this.client, msg.author);
+        } else {
+            console.error(
+                `message ${msg.id} trigging error in channel ${msg.channel.id}: permission check: both member and author values are falsey`
+            );
+            return msg.reply(
+                ":warning: unexpected condition occured: both member and author values are falsey"
+            );
+        }
+        if (cmd.permission > perm) {
+            return msg.reply(
+                ":warning: unauthorized: elevated permission required."
+            );
+        }
+
         if (stringArgs.length !== cmd.types.length)
             return msg.reply(
                 `:warning: expected ${cmd.types.length} arguments but got ${stringArgs.length} arguments instead`
@@ -61,11 +72,11 @@ export default class CommandParserModule extends Module {
         const typedArgs = [] as unknown[];
         for (const i in stringArgs) {
             const sa = stringArgs[i];
-            if (!types[cmd.types[i].name])
+            if (!this.types[cmd.types[i].name])
                 return msg.reply(
                     `:warning: command tried to use an unsupported argument type ${cmd.types[i].name}`
                 );
-            const arg = types[cmd.types[i].name](sa, msg);
+            const arg = this.types[cmd.types[i].name](sa, msg);
             if (arg === null || arg === undefined) {
                 return msg.reply(
                     `:warning: argument #${parseInt(i, 10) +
