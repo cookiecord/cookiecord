1d48c104d8096a393f5671df7057d004ce4929a5
Ron B <25263210+ronthecookie@users.noreply.github.com>
2019-10-29 14:43:13 +0200

    Custom arg types (#3)
    * Add custom arg types
    
    * make commandArgumentTypes not required
    
    * Update the TODO

--- src/client.ts
+++ src/client.ts
@@ -1,9 +1,10 @@
 import { Client, ClientOptions } from "discord.js";
 import CommandManager from "./command/commandManager";
 import ListenerManager from "./listener/listenerManager";
-import CommandParserModule from "./command/commandParser";
+import CommandParserModule, { ArgTypes } from "./command/commandParser";
 interface CookiecordOptions {
 	botAdmins?: string[];
+	commandArgumentTypes?: ArgTypes
 }
 export default class CookiecordClient extends Client {
 	public commandManager: CommandManager;
@@ -14,6 +15,6 @@ export default class CookiecordClient extends Client {
 		this.botAdmins = opts.botAdmins || []; 
 		this.commandManager = new CommandManager();
 		this.listenerManager = new ListenerManager(this);
-		new CommandParserModule(this);
+		new CommandParserModule(this, opts.commandArgumentTypes || {});
 	}
 }
