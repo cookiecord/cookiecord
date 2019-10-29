f640a77672ccb64ce4c243d930c853ed9ebdeea2
RONTheCookie <me@ronthecookie.me>
2019-10-29 13:52:59 +0200

    make commandArgumentTypes not required

--- src/client.ts
+++ src/client.ts
@@ -4,17 +4,17 @@ import ListenerManager from "./listener/listenerManager";
 import CommandParserModule, { ArgTypes } from "./command/commandParser";
 interface CookiecordOptions {
 	botAdmins?: string[];
-	commandArgumentTypes: ArgTypes
+	commandArgumentTypes?: ArgTypes
 }
 export default class CookiecordClient extends Client {
 	public commandManager: CommandManager;
 	public listenerManager: ListenerManager;
 	public botAdmins: string[];
-	constructor(opts: CookiecordOptions = {commandArgumentTypes: {}}, discordOpts: ClientOptions = {}) { // look at the example lol wait!!! 
+	constructor(opts: CookiecordOptions = {}, discordOpts: ClientOptions = {}) { // look at the example lol wait!!! 
 		super(discordOpts);
 		this.botAdmins = opts.botAdmins || []; 
 		this.commandManager = new CommandManager();
 		this.listenerManager = new ListenerManager(this);
-		new CommandParserModule(this, opts.commandArgumentTypes);
+		new CommandParserModule(this, opts.commandArgumentTypes || {});
 	}
 }
