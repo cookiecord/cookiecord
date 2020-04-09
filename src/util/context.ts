c823840a5ba6d931d60af8072bdf566c14dc6383
Ron B <me@ronthecookie.me>
2020-04-09 20:31:09 +0300

    context def syntax sugar

--- src/util/context.ts
+++ src/util/context.ts
@@ -1,12 +1,9 @@
 import { Message } from "discord.js";
 
 export default class Context {
-    prefix: string;
-    trigger: string;
-    msg: Message;
-    constructor(msg: Message, prefix: string, trigger: string) {
-        this.msg = msg;
-        this.prefix = prefix;
-        this.trigger = trigger;
-    }
+    constructor(
+        public msg: Message,
+        public prefix: string,
+        public trigger: string
+    ) {}
 }
