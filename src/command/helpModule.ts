65a332c20f765c370c8a8e149e15d139db7e6392
Ron B <me@ronthecookie.me>
2020-09-03 14:22:10 +0300

    format code with prettier and add markdown formatting to description lines

--- src/command/helpModule.ts
+++ src/command/helpModule.ts
@@ -4,41 +4,41 @@ import Module from "../module";
 import { command } from "./decorator";
 import { getModuleCommands } from "../util/getModuleAssociates";
 
-
-
 export default class HelpModule extends Module {
-  constructor(client: CookiecordClient) {
-    super(client);
-  }
+    constructor(client: CookiecordClient) {
+        super(client);
+    }
 
-  @command()
-  async help(msg: Message) {
-    const prefix = await this.client.getPrefix(msg);
-    const modules = Array.from(this.client.modules).filter(
-      (mod) => getModuleCommands(mod).length !== 0
-    );
+    @command()
+    async help(msg: Message) {
+        const prefix = await this.client.getPrefix(msg);
+        const modules = Array.from(this.client.modules).filter(
+            mod => getModuleCommands(mod).length !== 0
+        );
 
-    const _commands = Array.from(this.client.commandManager.cmds);
+        const _commands = Array.from(this.client.commandManager.cmds);
 
-    const initialEmbed = new MessageEmbed()
-      .setTitle("Help")
-      .setTimestamp()
-      .setColor("RANDOM");
+        const initialEmbed = new MessageEmbed()
+            .setTitle("Help")
+            .setTimestamp()
+            .setColor("BLUE");
 
-    const embed = modules.reduce((embed, module) => {
-      const commands = _commands.filter((command) => command.module === module);
-      return embed.addField(
-        module.constructor.name,
-        commands.map((command) => {
-          const name = command.id.split("/")[1];
-          const description = command.description
-            ? `: ${command.description}`
-            : "";
-          return `${prefix}${name}${description}`;
-        })
-      );
-    }, initialEmbed);
+        const embed = modules.reduce((embed, module) => {
+            const commands = _commands.filter(
+                command => command.module === module
+            );
+            return embed.addField(
+                module.constructor.name,
+                commands.map(command => {
+                    const name = command.id.split("/")[1];
+                    const description = command.description
+                        ? `: *${command.description}*`
+                        : "";
+                    return `**${prefix}${name}**${description}`;
+                })
+            );
+        }, initialEmbed);
 
-    await msg.channel.send(embed);
-  }
+        await msg.channel.send(embed);
+    }
 }
