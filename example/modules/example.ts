b08aaa94261a5d71e869970e1123947be22e98f8
ckie <git-525ff67@ckie.dev>
2022-02-12 23:18:51 +0200

    example/example: fix !todo

--- example/modules/example.ts
+++ example/modules/example.ts
@@ -87,7 +87,7 @@ export default class ExampleModule extends Module {
     }
     @command({})
     todo(msg: Message) {
-        msg.channel.send("```" + readFileSync("../TODO").toString() + "```");
+        msg.channel.send("```" + readFileSync("../TODO").toString().substr(0, 1900) + "```");
     }
     @command({
         inhibitors: [
